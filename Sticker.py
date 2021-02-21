f07f1d9fc78b7e9514df89dc65d9d722c5dbdd58
Taehee <kairess87@gmail.com>
2021-02-21 21:22:57 +0900

    add stickers

--- Sticker.py
+++ Sticker.py
@@ -40,9 +40,14 @@ class Sticker(QtWidgets.QMainWindow):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
-    s3 = Sticker('gif/amongus/red.gif', x=2000, y=1300, size=0.3)
-    s4 = Sticker('gif/amongus/orange.gif', x=1000, y=1310, size=0.3)
-    s1 = Sticker('gif/left.gif', x=-80, y=200, on_top=True)
-    s2 = Sticker('gif/amongus/red_vent.gif', x=780, y=1200, size=0.3, on_top=True)
+    s = Sticker('gif/left.gif', x=-80, y=200, on_top=False)
+
+    s1 = Sticker('gif/amongus/red_vent.gif', x=780, y=1020, size=0.3, on_top=True)
+    s2 = Sticker('gif/amongus/orange.gif', x=1200, y=1020, size=0.3, on_top=True)
+    s3 = Sticker('gif/amongus/blue_green.gif', x=400, y=920, size=1.0, on_top=True)
+    s4 = Sticker('gif/amongus/mint.gif', x=1000, y=950, size=0.2, on_top=True)
+    s5 = Sticker('gif/amongus/brown.gif', x=200, y=1010, size=0.75, on_top=True)
+    s6 = Sticker('gif/amongus/yellow.gif', x=1850, y=800, size=0.75, on_top=True)
+    s7 = Sticker('gif/amongus/magenta.gif', x=1500, y=900, size=0.5, on_top=True)
 
     sys.exit(app.exec_())
